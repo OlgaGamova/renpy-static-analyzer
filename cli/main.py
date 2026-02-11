f9194ed752426e71d1b52565246dae67b1443f96
Olga Gamova <gamova.olga@icloud.com>
2026-02-12 00:28:56 +0500

    Add Graph Builder

--- cli/main.py
+++ cli/main.py
@@ -1,5 +1,7 @@
 from core.parser.parser import RenPyParser
 from core.parser.transformer import RenPyTransformer
+from core.graph.builder import GraphBuilder
+
 
 def main():
     parser = RenPyParser()
@@ -13,6 +15,14 @@ def main():
 
     print("\n=== IR MODEL ===")
     print(script)
+
+    builder = GraphBuilder()
+    graph = builder.build(script)
+
+    print("\n=== GRAPH ===")
+    for src, targets in graph.items():
+        for tgt in targets:
+            print(f"{src} -> {tgt}")
 
 if __name__ == "__main__":
     main()
---
+++ core/graph/builder.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+from core.ir.model import Script, Jump, Menu
+
+
+class GraphBuilder:
+    """
+    Строит ориентированный граф переходов между label'ами.
+    """
+
+    def build(self, script: Script) -> dict[str, set[str]]:
+        graph = defaultdict(set)
+
+        for label_name, label in script.labels.items():
+            graph[label_name]
+            self._walk_body(label_name, label.body, graph)
+
+        return dict(graph)
+
+    def _walk_body(self, current_label: str, body, graph):
+        for node in body:
+
+            # Прямой переход
+            if isinstance(node, Jump):
+                graph[current_label].add(node.target)
+
+            # Меню
+            elif isinstance(node, Menu):
+                for option in node.options:
+                    self._walk_body(current_label, option.body, graph)
